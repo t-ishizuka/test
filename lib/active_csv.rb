bc9f6eaf9e09304de57c96aa28ea3f8b5d79d0e3
snowfield702 <test@example.com>
2015-11-03 22:27:30 +0900

    first commit

---
+++ lib/active_csv.rb
@@ -0,0 +1,61 @@
+# coding: utf-8
+require "csv"
+
+# Description:
+#   CSVのデータをもとにモデルから参照するときに1件づつ参照すると時間がかかる
+#   しかし、全件まとめてモデルのインスタンスを生成するとメモリ消費が不安
+#   そこでCSVからの取得を指定件数づつ行うためのライブラリ
+# 
+# Example:
+#   # batch_sizeにまとめて取得する件数を指定
+#   ActiveCSV.bulk_foreach('test.csv', encoding: 'UTF-8', batch_size: 5) do |rows|
+#     # 指定したカラム番号のデータを配列で取得
+#     ids = rows.values(0)
+#
+#     # 例えばこんな感じに数件づつモデルから取得するのに利用
+#     SampleModel.where(id: ids).each do |model|
+#       # さらに処理中のデータに対応させることも可能
+#       row = rows.find_by_value(0, model.id)
+#     end
+#   end
+
+class ActiveCSV < CSV
+  class << self
+    def bulk_foreach(path, options = {})
+      batch_size = options.delete(:batch_size) || 20
+      read(path, options).each_slice(batch_size) do |rows|
+        yield(Rows.new(rows))
+      end
+    end
+  end
+
+  class Rows
+    include Enumerable
+
+    attr_reader :rows
+
+    def initialize(rows)
+      @rows = rows
+    end
+
+    def [](key)
+      @rows[key]
+    end
+
+    def each
+      @rows.each { |row| yield(row) }
+    end
+
+    def values(column_num)
+      rows.map { |row| row[column_num] }
+    end
+
+    def find_by_value(column_num, value)
+      rows.find { |row| row[column_num] == value } || []
+    end
+
+    def size
+      rows.size
+    end
+  end
+end
